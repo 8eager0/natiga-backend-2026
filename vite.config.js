55d2247d9967a29681cd2b1d9f64d5d05f5988c1
8eager0 <8eager0@github.com>
2026-07-22 21:49:30 +0300

    Add build timestamp to asset filenames to force instant browser cache refresh

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,14 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].${Date.now()}.js`,
+        chunkFileNames: `assets/[name].${Date.now()}.js`,
+        assetFileNames: `assets/[name].${Date.now()}.[ext]`
+      }
+    }
   }
 })
